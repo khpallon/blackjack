ace80d76a50891f78a0305c201b7ab8b65f222f3
Adag124 <131304228+Adag124@users.noreply.github.com>
2025-05-22 17:00:37 +0300

    Update Main.java

--- src/main/java/org/blackjack/Main.java
+++ src/main/java/org/blackjack/Main.java
@@ -137,7 +137,7 @@ public class Main extends Application {
         jätaNupp.setOnAction(e -> jätaKäik());
 
 
-
+        // paigutus
         VBox diilerBox = new VBox(new Label("Dealer:"), diileriAla);
         VBox.setVgrow(diileriAla, Priority.ALWAYS);
         diilerBox.setMaxWidth(Double.MAX_VALUE);
@@ -166,6 +166,7 @@ public class Main extends Application {
         mänguJuht.setCenter(kaardidBox);
         mänguJuht.setBottom(tegevusnupud);
 
+        // klaviatuuri nupuvajutused
         mänguStseen = new Scene(mänguJuht, 400, 250);
         mänguStseen.setOnKeyPressed(e -> tryCatch(() -> {
             if (e.getCode() == KeyCode.DIGIT1) {
@@ -371,7 +372,7 @@ public class Main extends Application {
     }
 
     /**
-     * Peameetod, mis käivitab JavaFX rakenduse.
+     * Peameetod, mis käivitab rakenduse.
      * @param args
      */
     public static void main(String[] args) {
